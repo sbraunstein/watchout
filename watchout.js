4ecb85355959b1c1fe5a55274b3192d4234b6420
MKS-Student <mks.student@hackreactor.com>
2015-05-01 13:19:02 -0700

    board layout almost done

--- watchout.js
+++ watchout.js
@@ -1,2 +1,10 @@
-// start slingin' some d3 here.
 
+ var circleData = [{ "cx": 20, "cy": 20, "radius": 20, "color" : "green" },
+{ "cx": 70, "cy": 70, "radius": 20, "color" : "purple" }];
+ 
+//Create the SVG Viewport
+ var svgContainer = d3.select("body").append("svg").attr("width",700).attr("height",450);
+
+//Add circles to the svgContainer
+var circles = svgContainer.selectAll("circle").data(circleData).enter().append("circle");//Add the circle attributes
+var circleAttributes = circles.attr("cx", function (d) { return d.cx; }).attr("cy", function (d) { return d.cy; }).attr("r", function (d) { return d.radius; }).style("fill", function (d) { return d.color; });
